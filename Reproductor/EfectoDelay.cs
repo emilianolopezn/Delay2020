691e099b11a7aaf0d1b58440a8dd4351d542b501
Armando Meza Leyva <armezaleyva@gmail.com>
2020-02-27 12:52:52 -0700

    Agregar archivos de proyecto.

---
+++ Reproductor/EfectoDelay.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NAudio.Wave;
+
+namespace Reproductor
+{
+    class EfectoDelay : ISampleProvider
+    {
+        private ISampleProvider fuente;
+
+        public int offsetMiliSegundos;
+        private List<float> muestras = new List<float>();
+        private int tamañoBuffer;
+
+        private int cantidadMuestrasTranscurridas = 0;
+        private int cantidadMuestrasBorradas = 0;
+
+
+        public EfectoDelay(ISampleProvider fuente, int offsetMiliSegundos)
+        {
+            this.fuente = fuente;
+            this.offsetMiliSegundos = offsetMiliSegundos;
+
+            tamañoBuffer = fuente.WaveFormat.SampleRate * 20 * 
+                fuente.WaveFormat.Channels;
+        }
+
+        public WaveFormat WaveFormat
+        {
+            get
+            {
+                return fuente.WaveFormat;
+            }
+        }
+
+        public int Read(float[] buffer, int offset, int count)
+        {
+            int read = fuente.Read(buffer, offset, count);
+
+
+            return read;
+        }
+    }
+}
